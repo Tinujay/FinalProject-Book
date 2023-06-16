01a2ac85be773cee9d370df4ed2ae0e106808d3d
run <tinuvazhayil@gmail.com>
2023-06-16 14:20:25 -0400

    created front page

--- views/default.jsx
+++ views/default.jsx
@@ -3,19 +3,22 @@ const React = require('react')
 function Def (html) {
     return (
         <html>
+            
             <head>
                 <title>BookReview</title>
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
+
             <body>
 
              {html.children}
 
              <footer>
-                <p>&copy; 2023 BookReview</p>
+                <p>&copy; 2023 Book Review</p>
             </footer>
             
             </body>
+
         </html>
     )
 }
--- views/homelogin.jsx
+++ views/homelogin.jsx
@@ -4,55 +4,58 @@ import Def from './default';
 const HomeLogin = () => {
   return (
     <Def>
-      <main
-        style={{
-          backgroundImage: "url(/images/movietheater.jpg)",
-          height: "100vh",
-          width: "100vw",
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}
-      >
-        <h1 className="hero-title">Cinema Central</h1>
-        <p className="hero-subtitle">Discover the latest movie reviews from our critics!</p>
-        <div className="hero-action">
-          <a href="/movies" className="btn btn-primary movie button">Explore</a>
+       <main
+            style={{
+                background: "url(/images/Bookshelf-cat.jpg)",
+                backgroundSize: "contain",
+                backgroundPosition: "center",
+                minHeight: "90vh",
+                minWidth: "90vw",    
+            }}           
+        >
+      
+        <div className="title">
+            <h1 className="login-title">Reading Diary</h1>
+            <p className="login-subtitle">Welcome! Login or Sign Up to log your books, write reviews and keep track of your reading journey. </p>
         </div>
+
         <div className="login-signup-section">
-          <h2>Login or Sign Up for Book Reviews</h2>
-          <div className="login-container">
-            <form>
-              <h3>Login</h3>
-              <div>
-                <label htmlFor="login-email">Email:</label>
-                <input type="email" id="login-email" name="email" />
-              </div>
-              <div>
-                <label htmlFor="login-password">Password:</label>
-                <input type="password" id="login-password" name="password" />
-              </div>
-              <button type="submit">Login</button>
-            </form>
-          </div>
-          <div className="signup-container">
-            <form>
-              <h3>Sign Up</h3>
-              <div>
-                <label htmlFor="signup-name">Name:</label>
-                <input type="text" id="signup-name" name="name" />
-              </div>
-              <div>
-                <label htmlFor="signup-email">Email:</label>
-                <input type="email" id="signup-email" name="email" />
-              </div>
-              <div>
-                <label htmlFor="signup-password">Password:</label>
-                <input type="password" id="signup-password" name="password" />
-              </div>
-              <button type="submit">Sign Up</button>
-            </form>
-          </div>
+
+            <div className="login-container">
+                    <form>
+                        <h3>Login</h3>
+                    <div>
+                        <label htmlFor="login-email">Email:</label>
+                        <input type="email" id="login-email" name="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="login-password">Password:</label>
+                        <input type="password" id="login-password" name="password" />
+                    </div>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+
+                <div className="signup-container">
+                    <form>
+                        <h3>Sign Up</h3>
+                    <div>
+                        <label htmlFor="signup-name">Name:</label>
+                        <input type="text" id="signup-name" name="name" />
+                    </div>
+                    <div>
+                        <label htmlFor="signup-email">Email:</label>
+                        <input type="email" id="signup-email" name="email" />
+                    </div>
+                    <div>
+                        <label htmlFor="signup-password">Password:</label>
+                        <input type="password" id="signup-password" name="password" />
+                    </div>
+                        <button type="submit">Sign Up</button>
+                    </form>
+                </div>
         </div>
+
       </main>
     </Def>
   );
